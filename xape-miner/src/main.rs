6d65d5057c14d92566b6194de4d7ca6b6cdc1d08
Levi Cook <levicook@gmail.com>
2021-12-18 20:17:12 -0700

    load blanks and entanglements...ffs

--- xape-miner/src/main.rs
+++ xape-miner/src/main.rs
@@ -6,6 +6,7 @@ use serde::Deserialize;
 use solana_client::rpc_client::RpcClient;
 use solana_sdk::{account::ReadableAccount, pubkey::Pubkey};
 use std::{error::Error, fmt::Debug, fs::File, io::BufRead, io::BufReader};
+use tokio::join;
 
 #[derive(Clone, Debug, Options)]
 struct Args {
@@ -16,9 +17,19 @@ struct Args {
 #[derive(Clone, Debug, Options)]
 enum Command {
     #[options(help = "load the mint files into sqlite")]
+    LoadBlanks(LoadBlanks),
+    #[options(help = "load the mint files into sqlite")]
     LoadMints(LoadMints),
     #[options(help = "populate entanglements table from mints")]
     LoadEntanglements(LoadEntanglements),
+}
+
+#[derive(Clone, Debug, Options)]
+struct LoadBlanks {
+    #[options(help = "blanks csv file")]
+    csv_file: String,
+    #[options(help = "sqlite db path")]
+    db: String,
 }
 
 #[derive(Clone, Debug, Options)]
@@ -40,15 +51,22 @@ struct LoadEntanglements {
 }
 
 #[derive(Clone, Debug, Deserialize)]
-struct JSONMeta {
+struct MircMeta {
     name: String,
-    attributes: Vec<JSONAttr>,
+    image: String,
+    attributes: Vec<MircAttr>,
 }
 
 #[derive(Clone, Debug, Deserialize)]
-struct JSONAttr {
+struct MircAttr {
     value: String,
     trait_type: String,
+}
+
+#[derive(Clone, Debug, Deserialize)]
+struct MonoMeta {
+    name: String,
+    image: String,
 }
 
 #[tokio::main]
@@ -57,26 +75,57 @@ async fn main() -> Result<(), Box<dyn Error>> {
     match args.clone().command {
         None => todo!(),
         Some(command) => match command {
-            Command::LoadMints(opts) => load_mints(args, opts).await,
-            Command::LoadEntanglements(opts) => load_entanglements(args, opts).await,
+            Command::LoadBlanks(opts) => load_blanks(opts).await,
+            Command::LoadEntanglements(opts) => load_entanglements(opts).await,
+            Command::LoadMints(opts) => load_mints(opts).await,
             // Command::MineMetas(opts) => mine_metas(args, opts),
         },
     }
 }
 
-async fn load_entanglements(_args: Args, opts: LoadEntanglements) -> Result<(), Box<dyn Error>> {
+async fn load_blanks(opts: LoadBlanks) -> Result<(), Box<dyn Error>> {
+    let db = Connection::open(opts.db)?;
+    db.execute("DROP TABLE IF EXISTS blanks", params![])?;
+    db.execute(
+        "CREATE TABLE blanks (
+             mono_mint   text primary key,
+             mirc_name   text unique,
+             mirc_number numeric unique
+        )",
+        params![],
+    )?;
+
+    let file = File::open(opts.csv_file)?;
+    let mut rdr = csv::Reader::from_reader(BufReader::new(file));
+    for result in rdr.records() {
+        let record = result?;
+        let mirc_name = record.get(0).unwrap();
+        let mono_mint = record.get(14).unwrap();
+        let mirc_number = mirc_name.strip_prefix("Exiled Ape #").unwrap_or("");
+        db.execute(
+            "INSERT INTO blanks
+            (mono_mint, mirc_name, mirc_number) values
+            (       ?1,        ?2,          ?3)",
+            params![mono_mint, mirc_name, mirc_number,],
+        )?;
+    }
+
+    Ok(())
+}
+
+async fn load_entanglements(opts: LoadEntanglements) -> Result<(), Box<dyn Error>> {
     let db = Connection::open(opts.db)?;
     db.execute("DROP TABLE IF EXISTS entanglements", params![])?;
     db.execute(
         "CREATE TABLE entanglements (
              mirc_mint_address text primary key,
              mono_mint_address text unique
-         )",
+        )",
         params![],
     )?;
 
     let mut stmt = db.prepare(
-        "SELECT mint_address, meta_address, meta_name, meta_uri, inmate_number
+        "SELECT mint_address, meta_address, meta_name, meta_uri, inmate_number, image_uri
              FROM mirc_mints
              ORDER BY mint_address",
     )?;
@@ -86,149 +135,68 @@ async fn load_entanglements(_args: Args, opts: LoadEntanglements) -> Result<(), 
         let mirc_row = mirc_row.unwrap();
 
         let mono_row = db.query_row(
-            "SELECT mint_address, meta_address, meta_name, meta_uri, inmate_number
-                FROM mono_mints
-                WHERE inmate_number like ?1
-                LIMIT 1
-            ",
+            "SELECT mint_address, meta_address, meta_name, meta_uri, inmate_number, image_uri
+                 FROM mono_mints
+                 WHERE inmate_number like ?1
+                 LIMIT 1",
             params![mirc_row.inmate_number],
             |row| try_mint_row(row),
         );
-        let mono_row = mono_row.unwrap();
 
-        // normal entanglement
-        db.execute(
-            "INSERT INTO entanglements
+        if let Ok(mono_row) = mono_row {
+            // normal entanglement
+            db.execute(
+                "INSERT INTO entanglements
                      (mirc_mint_address, mono_mint_address) values
                      (               ?1,                ?2)",
-            params![mirc_row.mint_address, mono_row.mint_address,],
-        )?;
+                params![mirc_row.mint_address, mono_row.mint_address,],
+            )?;
+        }
     }
 
     let mut stmt = db.prepare(
-        "SELECT mint_address, meta_address, meta_name, meta_uri, inmate_number
-             FROM mirc_mints
-             WHERE mint_address NOT IN (SELECT mirc_mint_address FROM entanglements)
-             ORDER BY mint_address",
+        "SELECT mono_mint, mirc_name, mirc_number
+             FROM blanks
+             ORDER BY mirc_number",
     )?;
 
-    // TODO expect 518-30 ghost entanglements here
-
-    let mirc_mint_iter = stmt.query_map([], |row| try_mint_row(row))?;
-    for mirc_row in mirc_mint_iter {
-        let mirc_row = mirc_row.unwrap();
-        let mono_row = db.query_row(
+    let blanks_iter = stmt.query_map([], |row| try_blank_row(row))?;
+    for blank_row in blanks_iter {
+        let blank_row = blank_row.unwrap();
+        let mirc_row = db.query_row(
             "SELECT mint_address, meta_address, meta_name, meta_uri, inmate_number
-                FROM mono_mints
-                WHERE mint_address NOT IN (SELECT mono_mint_address FROM entanglements)
-                ORDER BY genesis_order
-                LIMIT 1
-            ",
-            params![],
+                 FROM mirc_mints
+                 WHERE meta_name = ?1 
+                 LIMIT 1",
+            params![blank_row.mirc_name,],
             |row| try_mint_row(row),
         );
-        let mono_row = mono_row.unwrap();
+        let mirc_row = mirc_row.unwrap();
 
         // ghost entanglement
         db.execute(
             "INSERT INTO entanglements
-                     (mirc_mint_address, mono_mint_address) values
-                     (               ?1,                ?2)",
-            params![mirc_row.mint_address, mono_row.mint_address,],
+            (mirc_mint_address, mono_mint_address) values
+            (               ?1,                ?2)",
+            params![mirc_row.mint_address, blank_row.mono_mint],
         )?;
     }
-
-    // TODO expect 518 entanglements here
 
     Ok(())
 }
 
-async fn load_mints(_args: Args, opts: LoadMints) -> Result<(), Box<dyn Error>> {
-    let rpc = RpcClient::new(opts.rpc);
-    let db = Connection::open(opts.db)?;
+async fn load_mints(opts: LoadMints) -> Result<(), Box<dyn Error>> {
+    let (x, y) = join!(load_mono_mints(opts.clone()), load_mirc_mints(opts.clone()));
 
-    db.execute("DROP TABLE IF EXISTS mirc_mints", params![])?;
-    db.execute(
-        "CREATE TABLE mirc_mints (
-             mint_address text primary key,
-             meta_address text unique,
-             meta_name text,
-             meta_uri text,
-             inmate_number text
-         )",
-        params![],
-    )?;
-    let mirc_file = File::open(opts.mirc_file)?;
-    let mirc_reader = BufReader::new(mirc_file);
-    for line in mirc_reader.lines() {
-        let mint_address = line.unwrap().parse()?;
-        let meta_address = find_metadata_address(mint_address);
-        let metadata = rpc.get_account(&meta_address)?;
-        let metadata = Metadata::deserialize(&mut metadata.data())?;
-
-        let jm = reqwest::get(metadata.data.clone().uri)
-            .await?
-            .json::<JSONMeta>()
-            .await?;
-
-        let mut inmate_number = "".to_string();
-        for attribute in jm.attributes {
-            if attribute.trait_type == "Inmate number" {
-                inmate_number = attribute.value;
-            }
+    if x.is_err() || y.is_err() {
+        let mut msg: Vec<String> = vec![];
+        if x.is_err() {
+            msg.push(format!("load_mono_mints: {:?}", x));
         }
-        db.execute(
-            "INSERT INTO mirc_mints
-            (mint_address, meta_address, meta_name, meta_uri, inmate_number) values
-            (          ?1,           ?2,        ?3,       ?4,            ?5)",
-            params![
-                mint_address.to_string(),
-                meta_address.to_string(),
-                metadata.data.name,
-                metadata.data.uri,
-                inmate_number,
-            ],
-        )?;
-    }
-
-    db.execute("DROP TABLE IF EXISTS mono_mints", params![])?;
-    db.execute(
-        "CREATE TABLE mono_mints (
-             mint_address text primary key,
-             meta_address text unique,
-             meta_name text,
-             meta_uri text,
-             inmate_number text,
-             genesis_order integer
-         )",
-        params![],
-    )?;
-    let mono_file = File::open(opts.mono_file)?;
-    let mono_reader = BufReader::new(mono_file);
-
-    let mut genesis_order = 0;
-    for line in mono_reader.lines() {
-        genesis_order = genesis_order + 1;
-
-        let mint_address = line.unwrap().parse()?;
-        let meta_address = find_metadata_address(mint_address);
-        let metadata = rpc.get_account(&meta_address)?;
-        let metadata = Metadata::deserialize(&mut metadata.data())?;
-        let inmate_number = metadata.data.name.strip_prefix("Degen Ape #").unwrap_or("");
-
-        db.execute(
-            "INSERT INTO mono_mints
-            (mint_address, meta_address, meta_name, meta_uri, inmate_number, genesis_order) values
-            (          ?1,           ?2,        ?3,       ?4,            5?,            ?6)",
-            params![
-                mint_address.to_string(),
-                meta_address.to_string(),
-                metadata.data.name,
-                metadata.data.uri,
-                inmate_number,
-                genesis_order,
-            ],
-        )?;
+        if y.is_err() {
+            msg.push(format!("load_mirc_mints: {:?}", y));
+        }
+        return Err(msg.join("\n").into());
     }
 
     Ok(())
@@ -247,12 +215,26 @@ fn find_metadata_address(mint: Pubkey) -> Pubkey {
 }
 
 #[derive(Debug)]
+struct BlankRow {
+    mono_mint: String,
+    mirc_name: String,
+}
+
+fn try_blank_row(row: &rusqlite::Row) -> Result<BlankRow, rusqlite::Error> {
+    Ok(BlankRow {
+        mono_mint: row.get(0)?,
+        mirc_name: row.get(1)?,
+    })
+}
+
+#[derive(Debug)]
 struct MintRow {
     mint_address: String,
     meta_address: String,
     meta_name: String,
     meta_uri: String,
     inmate_number: String,
+    image_uri: String,
 }
 
 fn try_mint_row(row: &rusqlite::Row) -> Result<MintRow, rusqlite::Error> {
@@ -262,10 +244,123 @@ fn try_mint_row(row: &rusqlite::Row) -> Result<MintRow, rusqlite::Error> {
         meta_name: row.get(2)?,
         meta_uri: row.get(3)?,
         inmate_number: row.get(4)?,
+        image_uri: row.get(5)?,
     };
     Ok(MintRow {
         meta_name: mint_row.meta_name.trim_matches(char::from(0)).to_string(),
         meta_uri: mint_row.meta_uri.trim_matches(char::from(0)).to_string(),
         ..mint_row
     })
+}
+
+async fn load_mirc_mints(opts: LoadMints) -> Result<(), Box<dyn Error>> {
+    let rpc = RpcClient::new(opts.rpc);
+    let db = Connection::open(opts.db)?;
+
+    db.execute("DROP TABLE IF EXISTS mirc_mints", params![])?;
+    db.execute(
+        "CREATE TABLE mirc_mints (
+             mint_address text primary key,
+             meta_address text unique,
+             meta_name text,
+             meta_uri text,
+             inmate_number text,
+             image_uri text
+         )",
+        params![],
+    )?;
+    let mirc_file = File::open(opts.mirc_file)?;
+    let mirc_reader = BufReader::new(mirc_file);
+    for line in mirc_reader.lines() {
+        let mint_address = line.unwrap().parse()?;
+        let meta_address = find_metadata_address(mint_address);
+        let metadata = rpc.get_account(&meta_address)?;
+        let metadata = Metadata::deserialize(&mut metadata.data())?;
+
+        let jm = reqwest::get(metadata.data.clone().uri)
+            .await?
+            .json::<MircMeta>()
+            .await?;
+
+        let mut inmate_number = "".to_string();
+        for attribute in jm.attributes {
+            if attribute.trait_type == "Inmate number" {
+                inmate_number = attribute.value;
+            }
+        }
+
+        db.execute(
+            "INSERT INTO mirc_mints
+            (mint_address, meta_address, meta_name, meta_uri, inmate_number, image_uri) values
+            (          ?1,           ?2,        ?3,       ?4,            ?5,        ?6)",
+            params![
+                mint_address.to_string(),
+                meta_address.to_string(),
+                metadata.data.name,
+                metadata.data.uri,
+                inmate_number,
+                jm.image,
+            ],
+        )?;
+    }
+
+    Ok(())
+}
+
+async fn load_mono_mints(opts: LoadMints) -> Result<(), Box<dyn Error>> {
+    let rpc = RpcClient::new(opts.rpc);
+    let db = Connection::open(opts.db)?;
+
+    db.execute("DROP TABLE IF EXISTS mono_mints", params![])?;
+    db.execute(
+        "CREATE TABLE mono_mints (
+             mint_address text primary key,
+             meta_address text unique,
+             meta_name text,
+             meta_uri text,
+             inmate_number text,
+             image_uri text,
+             genesis_order integer
+         )",
+        params![],
+    )?;
+    let mono_file = File::open(opts.mono_file)?;
+    let mono_reader = BufReader::new(mono_file);
+
+    let mut genesis_order = 0;
+    for line in mono_reader.lines() {
+        genesis_order = genesis_order + 1;
+
+        let mint_address = line.unwrap().parse()?;
+        let meta_address = find_metadata_address(mint_address);
+        let metadata = rpc.get_account(&meta_address)?;
+        let metadata = Metadata::deserialize(&mut metadata.data())?;
+        let inmate_number = metadata.data.name.strip_prefix("Degen Ape #").unwrap_or("");
+
+        let mut image = "".to_string();
+        match reqwest::get(metadata.data.clone().uri).await {
+            Err(e) => eprintln!("a {:?}", e),
+            Ok(res) => match res.json::<MonoMeta>().await {
+                Ok(jm) => image = jm.image,
+                Err(e) => eprintln!("b {:?}", e),
+            },
+        }
+
+        db.execute(
+            "INSERT INTO mono_mints
+            (mint_address, meta_address, meta_name, meta_uri, inmate_number, image_uri, genesis_order) values
+            (          ?1,           ?2,        ?3,       ?4,            ?5,        ?6,            ?7)",
+            params![
+                mint_address.to_string(),
+                meta_address.to_string(),
+                metadata.data.name,
+                metadata.data.uri,
+                inmate_number,
+                image,
+                genesis_order,
+            ],
+        )?;
+    }
+
+    Ok(())
 }
